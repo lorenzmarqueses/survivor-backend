4faecbba237c382c5564bc69b39dce75acf7f595
lorenzmarqueses <lorenz.marqueses22@gmail.com>
2025-01-09 18:26:19 +0800

    exclude AppController from Swagger documentation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiExcludeController } from '@nestjs/swagger';
 
+@ApiExcludeController() // This excludes the controller from Swagger docs
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
